01d7c6a3715a07c0b93e3afd6e1b30a31b040444
Kauê <kurpfy@gmail.com>
2021-01-06 14:52:29 -0300

    fixed SQLConnectionProvider missing class

--- common-shared/src/main/java/io/github/aluria/common/database/provider/sql/SQLConnectionProvider.java
+++ common-shared/src/main/java/io/github/aluria/common/database/provider/sql/SQLConnectionProvider.java
@@ -1,32 +1,26 @@
-package io.github.aluria.common.sql.provider;
+package io.github.aluria.common.database.provider.sql;
 
-
+import io.github.aluria.common.database.provider.ConnectionProvider;
 import io.github.aluria.common.sql.function.SqlFunction;
 import io.github.aluria.common.sql.mapper.SqlEntryMapper;
-import lombok.Getter;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public abstract class ConnectionProvider {
+public interface SQLConnectionProvider extends ConnectionProvider<Connection> {
 
-    @Getter
-    private final Properties properties;
+    default boolean execute(String sql, Object... values) {
+        final Connection connection = this.getConnectionInstance();
 
-    public ConnectionProvider(Properties properties) {
-        this.properties = properties;
-    }
-
-    public abstract Connection getCurrentConnection();
-
-    public boolean execute(String sql, Object... statementValues) {
-        Connection connection = this.getCurrentConnection();
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            applyValuesToStatement(statement, statementValues);
+            applyValuesToStatement(statement, values);
 
             return statement.execute();
         } catch (Exception e) {
@@ -35,21 +29,22 @@ public abstract class ConnectionProvider {
         }
     }
 
-    public int executeUpdate(String sql, Object... statementValues) {
-        Connection connection = this.getCurrentConnection();
+    default int executeUpdate(String sql, Object... statementValues) {
+        final Connection connection = this.getConnectionInstance();
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             this.applyValuesToStatement(preparedStatement, statementValues);
 
             return preparedStatement.executeUpdate();
+
         } catch (Exception e) {
             e.printStackTrace();
             return -1;
         }
     }
 
-    public ResultSet executeQuery(String sql, Object... statementValues) {
-        try (PreparedStatement statement = this.getCurrentConnection().prepareStatement(sql)) {
+    default ResultSet executeQuery(String sql, Object... statementValues) {
+        try (PreparedStatement statement = this.getConnectionInstance().prepareStatement(sql)) {
             applyValuesToStatement(statement, statementValues);
 
             return statement.executeQuery();
@@ -58,10 +53,10 @@ public abstract class ConnectionProvider {
         }
     }
 
-    public <T> List<T> selectAsList(String sql, SqlFunction<ResultSet, T> function, Object... statementValues) {
-        Connection currentConnection = getCurrentConnection();
+    default <T> List<T> selectAsList(String sql, SqlFunction<ResultSet, T> function, Object... statementValues) {
+        final Connection currentConnection = getConnectionInstance();
+        final List<T> collected = Collections.synchronizedList(new ArrayList<>());
 
-        List<T> collected = Collections.synchronizedList(new ArrayList<>());
         try (PreparedStatement statement = currentConnection.prepareStatement(sql)) {
             applyValuesToStatement(statement, statementValues);
 
@@ -81,8 +76,8 @@ public abstract class ConnectionProvider {
         return collected;
     }
 
-    public <T> T getFirstFromQuery(String sql, SqlFunction<ResultSet, T> function, Object... statementValues) {
-        Connection currentConnection = this.getCurrentConnection();
+    default <T> T getFirstFromQuery(String sql, SqlFunction<ResultSet, T> function, Object... statementValues) {
+        final Connection currentConnection = this.getConnectionInstance();
 
         try (PreparedStatement statement = currentConnection.prepareStatement(sql)) {
             applyValuesToStatement(statement, statementValues);
@@ -99,8 +94,8 @@ public abstract class ConnectionProvider {
         }
     }
 
-    public <K, V> Map<K, V> mapFromQuery(String sql, SqlEntryMapper<K, V> mapper, Object... statementValues) {
-        Map<K, V> map = new ConcurrentHashMap<>();
+    default <K, V> Map<K, V> mapFromQuery(String sql, SqlEntryMapper<K, V> mapper, Object... statementValues) {
+        final Map<K, V> map = new ConcurrentHashMap<>();
 
         try (ResultSet resultSet = executeQuery(sql, statementValues)) {
 
@@ -118,7 +113,7 @@ public abstract class ConnectionProvider {
         }
     }
 
-    private void applyValuesToStatement(PreparedStatement statement, Object... values) throws SQLException {
+    default void applyValuesToStatement(PreparedStatement statement, Object... values) throws SQLException {
         if (values.length > 0) {
             for (int i = 0; i < values.length; i++) {
                 statement.setObject(i + 1, values[i]);
