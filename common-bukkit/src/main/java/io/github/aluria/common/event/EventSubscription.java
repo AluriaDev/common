7a9e48cdd7eaf7bb8161a4b4761dd69bb6efaa2d
sasuked <majinboo.email@gmail.com>
2021-01-24 08:00:51 -0300

    ProtocolLib and Google Mug added.

--- common-bukkit/src/main/java/io/github/aluria/common/event/EventSubscription.java
+++ common-bukkit/src/main/java/io/github/aluria/common/event/EventSubscription.java
@@ -12,116 +12,116 @@ import java.util.function.Predicate;
 @SuppressWarnings("unchecked")
 public class EventSubscription<E extends Event> {
 
-    private static final Consumer<? extends Event> DEFAULT_CONSUMER = e -> {
-    };
+  private static final Consumer<? extends Event> DEFAULT_CONSUMER = e -> {
+  };
 
-    private Class<E> clazz;
-    private Predicate<E> filter;
+  private Class<E> clazz;
+  private Predicate<E> filter;
 
-    private Consumer<E> action;
+  private Consumer<E> action;
 
-    private boolean expireAfterExecute;
-    private int executions;
+  private boolean expireAfterExecute;
+  private int executions;
 
-    private EventPriority priority;
-    private boolean ignoreCancelled;
+  private EventPriority priority;
+  private boolean ignoreCancelled;
 
 
-    private EventSubscription(Class<E> clazz) {
-        this.clazz = clazz;
-        this.filter = Objects::nonNull;
-        this.expireAfterExecute = false;
-        this.executions = -1;
-        this.priority = EventPriority.NORMAL;
-        this.ignoreCancelled = false;
+  private EventSubscription(Class<E> clazz) {
+    this.clazz = clazz;
+    this.filter = Objects::nonNull;
+    this.expireAfterExecute = false;
+    this.executions = -1;
+    this.priority = EventPriority.NORMAL;
+    this.ignoreCancelled = false;
+  }
+
+  public static <T extends Event> EventSubscription<T> of(Class<T> clazz) {
+    return new EventSubscription<>(clazz);
+  }
+
+  public EventSubscription<E> filter(Predicate<E> predicate) {
+    filter = filter.and(predicate);
+    return this;
+  }
+
+  public EventSubscription<E> expireAfterExecute(int executions) {
+    this.expireAfterExecute = true;
+    this.executions = executions;
+    return this;
+  }
+
+  public EventSubscription<E> priority(EventPriority priority) {
+    this.priority = priority;
+    return this;
+  }
+
+  public EventSubscription<E> ignoreCancelled(boolean ignoreCancelled) {
+    this.ignoreCancelled = ignoreCancelled;
+    return this;
+  }
+
+
+  public EventSubscription<E> handler(Consumer<E> consumer) {
+    if (action == null) {
+      action = consumer;
+    } else {
+      action = action.andThen(consumer);
     }
 
-    public static <T extends Event> EventSubscription<T> of(Class<T> clazz) {
-        return new EventSubscription<>(clazz);
+    return this;
+  }
+
+  public EventSubscription<E> cancel() {
+    return handler(e -> {
+      if (e instanceof Cancellable) {
+        ((Cancellable) e).setCancelled(true);
+      }
+    });
+  }
+
+  public void listen(Plugin plugin) {
+    EventListenerExecutor<E> executor = new EventListenerExecutor<>(this);
+    Bukkit.getPluginManager().registerEvent(clazz, executor, priority, executor, plugin, ignoreCancelled);
+  }
+
+  private static class EventListenerExecutor<I extends Event> implements Listener, EventExecutor {
+
+    private EventSubscription<I> builder;
+
+    private EventListenerExecutor(EventSubscription<I> builder) {
+      this.builder = builder;
     }
 
-    public EventSubscription<E> filter(Predicate<E> predicate) {
-        filter = filter.and(predicate);
-        return this;
-    }
+    @Override
+    public void execute(Listener listener, Event e) throws EventException {
+      if (!builder.clazz.isInstance(e)) {
+        return;
+      }
 
-    public EventSubscription<E> expireAfterExecute(int executions) {
-        this.expireAfterExecute = true;
-        this.executions = executions;
-        return this;
-    }
+      I event = builder.clazz.cast(e);
+      if (builder.filter.negate().test(event)) {
+        return;
+      }
 
-    public EventSubscription<E> priority(EventPriority priority) {
-        this.priority = priority;
-        return this;
-    }
+      Consumer<I> action = builder.action;
+      if (action != null) {
+        action.accept(event);
+      }
 
-    public EventSubscription<E> ignoreCancelled(boolean ignoreCancelled) {
-        this.ignoreCancelled = ignoreCancelled;
-        return this;
-    }
-
-
-    public EventSubscription<E> handler(Consumer<E> consumer) {
-        if (action == null) {
-            action = consumer;
+      if (builder.expireAfterExecute) {
+        if (builder.executions > 1) {
+          builder.executions--;
         } else {
-            action = action.andThen(consumer);
+          unregister();
         }
-
-        return this;
+      }
     }
 
-    public EventSubscription<E> cancel() {
-        return handler(e -> {
-            if (e instanceof Cancellable) {
-                ((Cancellable) e).setCancelled(true);
-            }
-        });
+    public void unregister() {
+      for (HandlerList handlerList : HandlerList.getHandlerLists()) {
+        handlerList.unregister(this);
+      }
     }
-
-    public void listen(Plugin plugin) {
-        EventListenerExecutor<E> executor = new EventListenerExecutor<>(this);
-        Bukkit.getPluginManager().registerEvent(clazz, executor, priority, executor, plugin, ignoreCancelled);
-    }
-
-    private static class EventListenerExecutor<I extends Event> implements Listener, EventExecutor {
-
-        private EventSubscription<I> builder;
-
-        private EventListenerExecutor(EventSubscription<I> builder) {
-            this.builder = builder;
-        }
-
-        @Override
-        public void execute(Listener listener, Event e) throws EventException {
-            if (!builder.clazz.isInstance(e)) {
-                return;
-            }
-
-            I event = builder.clazz.cast(e);
-            if (builder.filter.negate().test(event)) {
-                return;
-            }
-
-            Consumer<I> action = builder.action;
-            if (action != null) {
-                action.accept(event);
-            }
-
-            if (builder.expireAfterExecute) {
-                if (builder.executions > 1) {
-                    builder.executions--;
-                } else {
-                    unregister();
-                }
-            }
-        }
-
-        public void unregister() {
-            for (HandlerList handlerList : HandlerList.getHandlerLists()) {
-                handlerList.unregister(this);
-            }
-        }
-    }
+  }
 }
